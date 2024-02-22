8adbcf408d649d713b49aa9cea491a9933e4d950
ronphilip25 <sanchezronphilip@gmail.com>
2024-02-22 14:16:48 +0800

    90% system

---
+++ accounts/choice.py
@@ -0,0 +1,10 @@
+STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
